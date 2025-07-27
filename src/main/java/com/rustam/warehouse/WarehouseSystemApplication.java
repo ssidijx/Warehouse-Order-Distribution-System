0b9b4a2aae7f9f463c4e4659f94d5500867ae73b
Rustam <ssidijx@gmail.com>
2025-07-27 16:01:54 +0300

    changes

---
+++ src/main/java/com/rustam/warehouse/WarehouseSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rustam.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseSystemApplication.class, args);
+	}
+
+}
